898f766220765ab0c67b2bdcaf663ace9642752c
Makselyanov <140087245+Makselyanov@users.noreply.github.com>
2025-11-24 15:25:11 -0800

    Trigger rebuild to fix missing Layout import error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
+// Main layout component
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
